3d210b7c5c465843ad9fa7922a05a4afbc9230a3
qasimraheem <qasimmehmood13936@gmail.com>
2018-04-19 21:21:54 +0500

    vr

---
+++ thehallaframe-master/thehallaframe-master/main.js
@@ -0,0 +1,7 @@
+<application>
+  <component name="MaterialCustomThemeConfig">
+    <option name="treeSelectionColor" value="c5000e" />
+    <option name="highlightColor" value="425b67" />
+    <option name="secondBorderColor" value="2a373e" />
+  </component>
+</application>
