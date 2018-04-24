ba1bd730d86e872b24976a84a0b35d3b18a4e3bf
qasimraheem <qasimmehmood13936@gmail.com>
2018-04-24 19:56:15 +0500

    testing of 3d object in vr web with textures

--- thehallaframe-master/thehallaframe-master/main.js
+++
@@ -1,7 +0,0 @@
-<application>
-  <component name="MaterialCustomThemeConfig">
-    <option name="treeSelectionColor" value="c5000e" />
-    <option name="highlightColor" value="425b67" />
-    <option name="secondBorderColor" value="2a373e" />
-  </component>
-</application>
